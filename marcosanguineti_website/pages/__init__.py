7ef4a9b2dfefc754441ac5d95fcb8440d6144d7c
GitMarco27 <marco.sanguineti.info@gmail.com>
2024-08-22 19:02:43 +0200

    Removing everything...

--- marcosanguineti_website/pages/__init__.py
+++
@@ -1,9 +0,0 @@
-from marcosanguineti_website.pages import (
-    contacts,
-    homepage,
-    page,
-    publications,
-    work_experience,
-)
-
-__all__ = ["page", "homepage", "contacts", "publications", "work_experience"]
