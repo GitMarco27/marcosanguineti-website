2711a45012bee943f4f9a284238147cb1af319e7
GitMarco27 <marco.sanguineti.info@gmail.com>
2024-08-23 19:23:18 +0200

    Bruce Wayne

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-container">
+        <img src="/ms.png" alt="alt" className="centered-image" />
+
+        <h1>Marco Sanguineti</h1>
+        <h2>AI Engineer | Python Developer</h2>
+        <h3>⚠️ This website is under construction ⚠️</h3>
+
+
+        <div className="contact-icons">
+            <a href="https://www.linkedin.com/in/marco-sanguineti/" className="linkedin">
+            <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/GitMarco27" className="github">
+            <i className="fab fa-github"></i>
+            </a>
+            <a href="https://medium.com/@marcosanguineti" className="medium">
+            <i className="fab fa-medium"></i>
+            </a>
+            <a href="https://www.youtube.com/@marcosanguineti2710" className="youtube">
+            <i className="fab fa-youtube"></i>
+            </a>
+            <a href="https://scholar.google.com/citations?user=hZHz6y4AAAAJ&hl=en" className="scholar">
+            <i className="fas fa-graduation-cap"></i>
+            </a>
+            <a href="https://github.com/GitMarco27/marcosanguineti-website" className="source">
+            <i className="fa-solid fa-code"></i>
+            </a>
+        </div>
+        </div>
+  );
+}
+
+export default Home;
